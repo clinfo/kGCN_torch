3e1845764e6ee6650826b2ea990df786d078ca2d
0h-n0 <kbu94982@gmail.com>
2020-11-20 14:09:15 +0900

    feat: add mnist test on graph module.

--- tests/nn/graph/test_networkx_module.py
+++ tests/nn/graph/test_networkx_module.py
@@ -1,5 +1,9 @@
+import time
+import warnings
+
 import torch
 import torch.nn as nn
+import pytest
 
 from kgcn_torch.nn.graph.networkx_module import NNGraph
 from kgcn_torch.nn.graph.networkx_module import GraphModule
@@ -30,6 +34,71 @@ def test_graph_module_with_loop_sequence_graph():
     nngraph.add_node('linear3', nn.Linear(32, 32), previous=['linear2'])
     x = torch.randn(16, 128)
     out = nngraph(x)
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='cuda is not available')
+def test_to_gpu():
+    nngraph = GraphModule()
+    nngraph.add_node('linear', nn.Linear(128, 64))
+    nngraph.add_node('linear1', nn.Linear(64, 64), previous=['linear'])
+    nngraph.add_node('linear2', nn.Linear(64, 32), previous=['linear1'])
+    nngraph.add_node('linear3', nn.Linear(32, 32), previous=['linear2'])
+    nngraph.to('cuda')
+    x = torch.randn(16, 128).to('cuda')
+    out = nngraph(x)
+    assert next(nngraph.parameters()).is_cuda
+    
+    
+@pytest.mark.skipif(not torch.cuda.is_available(), reason='cuda is not available')
+def test_mnist_performance():
+    import torchex.nn as exnn
+    import torch.nn.functional as F
+    import torch.optim as optim
+    from torchvision import datasets, transforms
+    device = torch.device("cuda")
+    nngraph = GraphModule()
+    f = exnn.Flatten()
+    nngraph.add_node('flatten', exnn.Flatten())    
+    nngraph.add_node('linear', nn.Linear(784, 128), previous=['flatten'])
+    nngraph.add_node('relu', nn.ReLU(), previous=['linear'])    
+    nngraph.add_node('linear1', nn.Linear(128, 64), previous=['relu'])
+    nngraph.add_node('relu1', nn.ReLU(), previous=['linear1'])    
+    nngraph.add_node('linear2', nn.Linear(64, 10), previous=['relu1'])
+
+    model = nngraph
+    model.to(device)
+    lr = 0.001
+    batch_size = 128
+    kwargs = {'num_workers': 1, 'pin_memory': True} if True else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    model.train()
+    for epoch in range(2):
+        total_loss = 0
+        correct = 0
+        for batch_idx, (data, target) in enumerate(train_loader):
+            s = time.time()
+            data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            output = F.log_softmax(output, dim=1)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            output = F.softmax(output, dim=1)
+            pred = output.argmax(dim=1, keepdim=True)
+            total_loss += loss.detach().cpu().item()
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            e = time.time()
+        acc = 100. * correct / ((batch_idx + 1) * batch_size)
+    warnings.warn(f'mnist train acc = {acc}, one iteration time = {e - s} s')
+    assert acc > 90.
+    
 
 
     
